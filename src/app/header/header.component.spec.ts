2d3db7cb981e3e5c705470cbaa55a04a1b4571d4
saul <saul.yeponde@gmail.com>
2021-02-07 19:36:40 +0100

    Tests fixes
    Fixed the header component tests.
    Fixed the app component tests.
    Fixed the teams component tests.

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,10 +1,9 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
+  let headerComponent: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async () => {
@@ -17,39 +16,37 @@ describe('HeaderComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
+    headerComponent = fixture.componentInstance;
+    headerComponent.headerTitle = 'Teams builder tool';
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(headerComponent).toBeTruthy();
   });
 
   it('should render home tab link', () => {
-    const fixture = TestBed.createComponent(HeaderComponent);
-    fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('a#home-tab-link').textContent).toContain('Home');
   });
 
   it('should render teams tab link', () => {
-    const fixture = TestBed.createComponent(HeaderComponent);
-    fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('a#teams-tab-link').textContent).toContain('Teams');
   });
 
   it('should render players tab link', () => {
-    const fixture = TestBed.createComponent(HeaderComponent);
-    fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('a#players-tab-link').textContent).toContain('Players');
   });
 
   it('should render status tab link', () => {
-    const fixture = TestBed.createComponent(HeaderComponent);
-    fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('a#status-tab-link').textContent).toContain('Status');
+  });
+
+  it('should render the app title in the nav bar', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('nav a.navbar-brand').textContent).toContain(headerComponent.headerTitle);
   });
 });
