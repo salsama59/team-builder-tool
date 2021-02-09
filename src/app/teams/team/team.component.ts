a56652c6ae34a30923d0b477ceb465f3038fb393
saul <saul.yeponde@gmail.com>
2021-02-10 00:37:39 +0100

    Documentation implementaion
    Added compodoc to generate the documentation.
    Added the documentation in all the *.ts files.
    Added an entry to .gitignore file to ignore the documentation of the app.

--- src/app/teams/team/team.component.ts
+++ src/app/teams/team/team.component.ts
@@ -2,6 +2,10 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Team } from 'src/app/models/team.model';
 import { TeamsService } from 'src/app/services/teams.service';
 
+/**
+ * This class represent the team component.
+ * @implements OnInit
+ */
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
@@ -9,11 +13,23 @@ import { TeamsService } from 'src/app/services/teams.service';
 })
 export class TeamComponent implements OnInit {
 
+  /**
+   * The team id to display
+   * @type {number}
+   * @public
+   */
   @Input()
   public teamIdToDisplay: number = 0;
 
+  /**
+   * The current team to be displayed
+   */
   public currentTeam: Team | null = null;
 
+  /**
+   * Creates an instance of team component.
+   * @param teamsService  the teams service injected
+   */
   constructor(private teamsService: TeamsService) { }
 
   ngOnInit(): void {
--- src/app/teams/teams.component.ts
+++ src/app/teams/teams.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Team } from '../models/team.model';
 import { TeamsService } from '../services/teams.service';
-
+/**
+ * This class represent the teams component.
+ * @implements OnInit
+ */
 @Component({
   selector: 'app-teams',
   templateUrl: './teams.component.html',
@@ -9,8 +12,17 @@ import { TeamsService } from '../services/teams.service';
 })
 export class TeamsComponent implements OnInit {
 
+  /**
+   * The team list that will be displayed.
+   * @type {Array<Team> | null}
+   * @public
+   */
   public teams: Array<Team> | null = null;
 
+  /**
+   * Creates an instance of teams component.
+   * @param teamsService the teams service injected
+   */
   constructor(private teamsService: TeamsService) { }
 
   ngOnInit(): void {
