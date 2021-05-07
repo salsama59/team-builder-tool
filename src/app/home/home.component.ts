0c58b8bd442a9d3f16186b1425df495e6f56c529
saul <saul.yeponde@gmail.com>
2021-05-07 11:58:46 +0200

    auto redirect tests

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 /**
  * This class represent the home component
@@ -8,16 +8,23 @@ import { Component } from '@angular/core';
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+	ngOnInit(): void {
+		this.onRedirectAfterSometimesToStore();
+	}
 	onRedirectToStore() {
 		window.location.href =
 			'market://details?id=fr.oney.mobile.mescomptes&hl=fr&gl=US';
 	}
 
 	onRedirectAfterSometimesToStore() {
+		console.log('Before set timeout');
 		setTimeout(() => {
+			console.log('Before redirect');
 			window.location.href =
 				'market://details?id=fr.oney.mobile.mescomptes&hl=fr&gl=US';
+			console.log('After redirect');
 		}, 5000);
+		console.log('After set timeout');
 	}
 }
