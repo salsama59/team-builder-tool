bf33cf0ee9265a2d1e9df2f99d925a16171e5e95
saul <saul.yeponde@gmail.com>
2021-05-07 10:36:02 +0200

    Intent implementation
    Will be detroyed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.css']
 })
-export class HomeComponent {}
+export class HomeComponent {
+	onRedirectToStore() {
+		window.location.href =
+			'market://details?id=fr.oney.mobile.mescomptes&hl=fr&gl=US';
+	}
+}
